9c933daf38677abd3243e464f1aa94337479f484
unknown <belongbelong223@gmail.com>
2018-09-28 00:57:45 +0800

    'test'

--- helloworld/views.py
+++ helloworld/views.py
@@ -3,7 +3,13 @@ from django.contrib.auth import authenticate
 from django.contrib import auth
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+import random 
 
+# for i in xrange(1,15):
+# 	piclist.append(random.randint(0,1000))
 
 def index(request):
-	return render(request,'index2.html')
+	# random.seed(99)
+	# piclist = [random.randint(1,1000) for _ in range(15)]
+	piclist = [1,2,3,4,5]
+	return render(request,'index2.html',{'piclist':piclist})
